cd7bd16b04a2d978861aa5d7e28059fc73683808
Mutasimos <build.stonehenge@gmail.com>
2024-10-21 12:50:31 +0600

    The sky isn't the only limit :(

--- src/main/java/randommemes/commands/ModCommands.java
+++ src/main/java/randommemes/commands/ModCommands.java
@@ -2,9 +2,10 @@ package randommemes.commands;
 
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.FloatArgumentType;
-import com.mojang.brigadier.arguments.IntegerArgumentType;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
 
 public class ModCommands {
 	public static void init(CommandDispatcher<ServerCommandSource> dispatcher) {
@@ -20,14 +21,26 @@ public class ModCommands {
 	}
 
 	private static int scaleUp(ServerCommandSource source, float scale) {
+		if (scale > 100 || scale < 0.1)
+			return 0;
+
 		// Run command `/attribute @s minecraft:generic.scale base set {scale}`
 		// This will scale the player up by the specified amount
 		CommandManager cmdman = source.getServer().getCommandManager();
 		cmdman.executeWithPrefix(source, "attribute @s minecraft:generic.scale base set " + scale);
-		cmdman.executeWithPrefix(source, "attribute @s minecraft:generic.movement_speed base set " + scale / 10);
+		cmdman.executeWithPrefix(source, "attribute @s minecraft:generic.movement_speed base set " + scale);
 		cmdman.executeWithPrefix(source, "attribute @s minecraft:player.block_interaction_range base set " + scale);
 		cmdman.executeWithPrefix(source, "attribute @s minecraft:player.entity_interaction_range base set " + scale);
 		cmdman.executeWithPrefix(source, "attribute @s minecraft:generic.step_height base set " + scale);
+
+		// Clear the chat
+		ServerPlayerEntity player = source.getPlayer();
+		for (int i = 0; i < 100; i++) {
+			assert player != null;
+			player.sendMessage(Text.of(""), false);
+		}
+
+		source.sendMessage(Text.of("Size changed to " + scale + "!"));
 
 		return 1;
 	}
